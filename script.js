f0bcb8c07df75588de546459d4fa391eee75c425
Julian Fellyco <74457155+julianfellyco@users.noreply.github.com>
2025-07-24 16:53:52 +1000

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
 // Initialize Supabase
-import { createClient } from '@supabase/supabase-js'
-
-const supabaseUrl = 'https://axhvrjivepslousbzooi.supabase.co'
-const supabaseKey = process.env.SUPABASE_KEY
-const supabase = createClient(supabaseUrl, supabaseKey)
+const supabase = window.supabase.createClient(
+  'https://axhvrjivepslousbzooi.supabase.co',
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImF4aHZyaml2ZXBzbG91c2J6b29pIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTMzMzE0MDMsImV4cCI6MjA2ODkwNzQwM30.NtJPdCXZKgcXYz3whPgiVZC4_OTSHiBY4rDXcSFLPYs'
+);
 
 // DOM Elements
 const balanceEl = document.getElementById('balance');
